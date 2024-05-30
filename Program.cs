6e92a69455b958d9f2bc3e138bcefd933de326ed
mjfusa <mike.francis@live.com>
2024-05-30 10:07:02 -0700

    Switch to in-memory db

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+  builder.Services.AddDbContext<TodoContext>(opt =>
+      opt.UseInMemoryDatabase("TodoList"));
 //  builder.Services.AddDbContext<TodoContext>(opt =>
-//      opt.UseInMemoryDatabase("TodoList"));
- builder.Services.AddDbContext<TodoContext>(opt =>
-     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//      opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
